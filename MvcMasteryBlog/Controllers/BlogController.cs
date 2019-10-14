98f12addf36956580beaee7fce86605de9aa70b5
Taylor Hemphill <54860461+THemphill23@users.noreply.github.com>
2019-10-14 09:33:23 -0400

    More Views, Controllers, and Repos

---
+++ MvcMasteryBlog/Controllers/BlogController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMasteryBlog.Controllers
+{
+    public class BlogController : Controller
+    {
+        public ViewResult Index()
+        {
+            var model = blogRepo.GetAll();
+
+            return View(model);
+        }
+
+        public ViewResult Detail(int id)
+        {
+            var model = blogRepo.GetByID(id);
+
+            return View(model);
+        }
+
+    }
+}
---
+++ MvcMasteryBlog/Models/Blog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMasteryBlog.Models
+{
+    public class Blog
+    {
+        public int ID { get; set; }
+        public string Image { get; set; }
+        public string Title { get; set; }
+        public int Date { get; set; }
+        public string Body { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public string Tags { get; set; }
+
+        public Blog()
+        {
+        }
+
+        public Blog(int id, string image, string title, int date, string body,
+            string author, string genre, string tags)
+        {
+            this.ID = id;
+            this.Image = image;
+            this.Title = title;
+            this.Date = date;
+            this.Body = body;
+            this.Author = author;
+            this.Genre = genre;
+            this.Tags = tags;
+        }
+    }
+}
