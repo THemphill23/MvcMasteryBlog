ca724b08ae7319e3aac263e2cbf960745a5235b3
Taylor Hemphill <54860461+THemphill23@users.noreply.github.com>
2019-10-17 10:31:52 -0400

    More Style

--- MvcMasteryBlog/Models/Blog.cs
+++ MvcMasteryBlog/Models/Blog.cs
@@ -10,7 +10,7 @@ namespace MvcMasteryBlog.Models
         public int ID { get; set; }
         public string Image { get; set; }
         public string Title { get; set; }
-        public int Date { get; set; }
+        public DateTime DateTime { get; set; }
         public string Body { get; set; }
         public string Author { get; set; }
 
@@ -26,13 +26,13 @@ namespace MvcMasteryBlog.Models
         {
         }
 
-        public Blog(int id, string image, string title, int date, string body,
+        public Blog(int id, string image, string title, DateTime datetime, string body,
             string author/*, ICollection<Tag> tags*/)
         {
             ID = id;
             Image = image;
             Title = title;
-            Date = date;
+            DateTime = datetime;
             Body = body;
             Author = author;
             //Tags = tags;
