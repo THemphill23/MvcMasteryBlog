a5b64ec6dde7c8b9aa7e03fd2fe498d2d60a9883
Taylor Hemphill <54860461+THemphill23@users.noreply.github.com>
2019-10-19 14:31:39 -0400

    Added Test

---
+++ MvcMasteryBlog.Tests/BlogControllerTests.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NSubstitute;
+using Microsoft.AspNetCore.Mvc;
+using MvcMasteryBlog.Controllers;
+using MvcMasteryBlog.Models;
+using MvcMasteryBlog.Repositories;
+using Xunit;
+
+namespace MvcMasteryBlog.Tests
+{
+    public class BlogControllerTests
+    {
+        BlogController underTest;
+        IRepository<Blog> blogRepo;
+
+        public BlogControllerTests()
+        {
+            blogRepo = Substitute.For<IRepository<Blog>>();
+            underTest = new BlogController(blogRepo);
+        }
+
+        [Fact]
+        public void Index_Returns_View_Result()
+        {
+
+            var result = underTest.Index();
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void Index_Passes_All_Products_To_View()
+        {
+            var expectedProducts = new List<Blog>();
+            blogRepo.GetAll().Returns(expectedProducts);
+
+            var result = underTest.Index();
+
+            Assert.Equal(expectedProducts, result.Model);
+        }
+
+        [Fact]
+        public void Details_Returns_A_View()
+        {
+            var result = underTest.Details(1);
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void Details_Passes_Blog_To_View()
+        {
+            var expectedProducts = new Blog();
+            blogRepo.GetByID(1).Returns(expectedProducts);
+
+            var result = underTest.Details(1);
+
+            Assert.Equal(expectedProducts, result.Model);
+        }
+    }
+
+
+}
